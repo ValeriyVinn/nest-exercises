fea18b653b3cb172bae3101164f42bd48e2cbed1
Valeriy Svystun <valeriy.all@gmail.com>
2025-09-01 16:24:14 +0300

    prepare: to start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.mongodb.net/nest-exercises',
+    // ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
